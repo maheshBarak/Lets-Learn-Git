eae5a5d541f9f41e7abc80d9cdebc425c8433ad9
Mahesh Barak <mahesh74748@gmail.com>
2023-04-15 11:01:06 +0530

    adding message in sum.java

--- sum.java
+++ sum.java
@@ -7,7 +7,7 @@ public class sum {
         int num1 = sc.nextInt();
         System.out.println("Enter second number");
         int num2 = sc.nextInt();
-        System.out.println(num1 + num2);
+        System.out.println("The sum is " + (num1 + num2));
         sc.close();
     }
 }
